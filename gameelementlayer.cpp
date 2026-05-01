59126132d37e51a1fea96cefe3d6f5de529418f7
bravver <15662666213@163.com>
2026-05-01 23:57:04 +0800

    fix: remove FULL_PIPE size check to match Java scoring behavior

--- gameelementlayer.cpp
+++ gameelementlayer.cpp
@@ -62,9 +62,8 @@ void GameElementLayer::pipeBornLogic(Bird *bird) {
         const int SCORE_DISTANCE = Pipe::PIPE_WIDTH * 2 + Constant::HORIZONTAL_INTERVAL;
 
         if (lastPipe->isInFrame()) {
-            // Java: pipes.size() >= FULL_PIPE - 2 = 8
-            if (pipes.size() >= PipePool::FULL_PIPE - 2 &&
-                lastScoreX != lastPipe->getX() &&
+            // 得分检测：当 currentDistance 满足条件且这根管道还没得过分
+            if (lastScoreX != lastPipe->getX() &&
                 currentDistance <= SCORE_DISTANCE + Pipe::PIPE_WIDTH * 3 / 2) {
                 counter->score();
                 lastScoreX = lastPipe->getX();
